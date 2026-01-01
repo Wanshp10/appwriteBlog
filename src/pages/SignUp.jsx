e8abdbd5d909a31b9f6098dc511ba8e7034f85a5
Wanshp10 <paryaniwansh@gmail.com>
2026-01-01 05:38:05 +0530

    Sixth commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SignUp as SignupComponent} from '../component/index.js'
 
-function Signup() {
+function SignUp() {
   return (
     <div className='py-8'>
         <SignupComponent />
@@ -9,4 +9,4 @@ function Signup() {
   )
 }
 
-export default Signup
+export default SignUp
